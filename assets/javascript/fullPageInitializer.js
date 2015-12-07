25810a53a92da7bb4a7617abe0de6fe96e7ee1c4
Ribs <helloribs@gmail.com>
2015-12-07 17:49:16 -0600

    Servicios (prueba)

--- assets/javascript/fullPageInitializer.js
+++ assets/javascript/fullPageInitializer.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-		anchors:['main', 'services', 'process', 'us', 'clients', 'procesocontent', 'nosotroscontent'],
-		scrollOverflow: true,
-  scrollingSpeed: 1000,
-		afterLoad: function(anchorLink, index){
-            changeLogo(index);
-        }
-	});
-	resizeSlidebarContact();
-	resizeSlidebar();
-	toggleMenu(0);
-	toggleContact(0);
+ $('#fullpage').fullpage({
+  anchors:['main', 'services', 'process', 'us', 'clients', 'procesocontent', 'nosotroscontent'],
+  scrollOverflow: true,
+  scrollingSpeed: 700,
+  afterLoad: function(anchorLink, index){
+   changeLogo(index);
+  }
+ });
+ resizeSlidebarContact();
+ resizeSlidebar();
+ toggleMenu(0);
+ toggleContact(0);
 });
 
 $(window).resize(resizeSlidebar);
 
 function resizeSlidebar() {
-	$('.slidebar').outerHeight($(window).height());
-	$('.menu-container').css('margin-top', $(window).height() / 2 - $('.menu-container').height() / 2);
+ $('.slidebar').outerHeight($(window).height());
+ $('.menu-container').css('margin-top', $(window).height() / 2 - $('.menu-container').height() / 2);
 }
 
 function toggleMenu( duration ) {
-	state = menuStatus();
-	movement = state ? "+=600" : "-=600";
-toggleContactVisibility();
-	imageUrl = state ? "img/cerrar.png" : "img/menu.png";
-	$('.slidebar').animate({ right: movement }, duration);
-	$('.menu').attr("src", imageUrl);
+ state = menuStatus();
+ movement = state ? "+=600" : "-=600";
+ toggleContactVisibility();
+ imageUrl = state ? "img/cerrar.png" : "img/menu.png";
+ $('.slidebar').animate({ right: movement }, duration);
+ $('.menu').attr("src", imageUrl);
 }
 
 function changeLogo( index ) {
-	if (index == 3 || index == 5) {
-		$('.logo').attr('src', 'img/bitlab_obscuro.png');
-	} else {
-		$('.logo').attr('src', 'img/bitlab.png');
-	}
+ if (index == 3 || index == 5) {
+  $('.logo').attr('src', 'img/bitlab_obscuro.png');
+ } else {
+  $('.logo').attr('src', 'img/bitlab.png');
+ }
 }
 
 function toggleContactVisibility() {
@@ -46,16 +46,16 @@ function menuStatus() {
 }
 
 function toggleContact( duration ) {
-	state = ($('.slidebarContact').position().left >= $( window ).width());
-	movement = state ? "+=600" : "-=600";
-	$('.slidebarContact').animate({ right: movement }, duration);
-	$('.contact').attr("src", imageUrl);
+ state = ($('.slidebarContact').position().left >= $( window ).width());
+ movement = state ? "+=600" : "-=600";
+ $('.slidebarContact').animate({ right: movement }, duration);
+ $('.contact').attr("src", imageUrl);
  if (!menuStatus() && state) {
   toggleMenu();
  }
 }
 
 function resizeSlidebarContact() {
-	$('.slidebarContact').outerHeight($(window).height());
-	$('.contact-container').css('margin-top', $(window).height() / 2 - $('.contact-container').height() / 2);
+ $('.slidebarContact').outerHeight($(window).height());
+ $('.contact-container').css('margin-top', $(window).height() / 2 - $('.contact-container').height() / 2);
 }
---
+++ assets/javascript/servicios.js
@@ -0,0 +1,8 @@
+
+var uno = ["Título" , "Subtítulo" , "Contenido"]
+
+function show(){
+ document.write(uno[0], uno[1], uno[2]);
+}
+
+show();
